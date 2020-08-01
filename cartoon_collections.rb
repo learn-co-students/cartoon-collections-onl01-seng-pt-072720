ac866133bd11a22045e6f91153035b5de1e4f7f4
fentyhall <fentymhall@gmail.com>
2020-07-31 17:41:36 -0700

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.map { |veggie| veggie.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  return true if calls_long.each_with_index { |x, index| x[index].length > 4 } 
+  return false if calls_long.each { |x| x.length <= 4 }
 end
 
 def find_the_cheese# code an argument here
